0879ed4bfcb6f66850edc0c1dec07beb3746836c
tejandar-koli <tejandarkoli1@gmail.com>
2026-04-16 17:23:52 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,36 +1,27 @@
-num1 = float(input("Enter a number: "))
-op = input("Enter operation (+, -, *, /, //, %, ^): ")
-num2 = float(input("Enter a number: "))
+num1 = float(input("Enter number: "))
+num2 = float(input("Enter number: "))
+
+print("+ for addition")
+print("- for subtraction")
+print("* for multiplication")
+print("/ for division")
+
+op = input("Choose operation: ")
 
 if op == "+":
-    print(num1 + num2)
+    print("Result:", num1 + num2)
 
 elif op == "-":
-    print(num1 - num2)
+    print("Result:", num1 - num2)
 
 elif op == "*":
-    print(num1 * num2)
+    print("Result:", num1 * num2)
 
 elif op == "/":
     if num2 != 0:
-        print(num1 / num2)
+        print("Result:", num1 / num2)
     else:
         print("Cannot divide by zero")
-
-elif op == "//":
-    if num2 != 0:
-        print(num1 // num2)
-    else:
-        print("Cannot divide by zero")
-
-elif op == "%":
-    if num2 != 0:
-        print(num1 % num2)
-    else:
-        print("Cannot divide by zero")
-
-elif op == "^":
-    print(num1 ** num2)
 
 else:
     print("Invalid operation")
